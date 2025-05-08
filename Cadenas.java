7e00818900b59bfc23f442c90f9b7284c6319e9b
Eyve9ykecua7y <20243rd049@utez.edu.mx>
2025-05-08 11:39:05 -0600

    imprimi un nombre

---
+++ Cadenas.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Cadenas{
+    public static void main(String args[]){
+        Scanner almacenar = new almacenar(System.int);
+       String nombre="eliel";
+       system.out.println("Escribe tu nombre: ");
+       String nombre = almacenar.nextLine();
+
+       System.out.println("El nuevo nombre es: " + nombre);
+
+    }
+}
