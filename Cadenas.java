c98feabcb302e2aa09b354c5d246908b65cd9b8b
Eyve9ykecua7y <20243rd049@utez.edu.mx>
2025-05-08 11:46:41 -0600

    corregi el codigo

--- Cadenas.java
+++ Cadenas.java
@@ -1,12 +1,11 @@
 import java.util.Scanner;
 public class Cadenas{
     public static void main(String args[]){
-        Scanner almacenar = new almacenar(System.int);
-       String nombre="eliel";
-       system.out.println("Escribe tu nombre: ");
+        Scanner almacenar = new Scanner(System.in);
+       System.out.println("Escribe tu nombre: ");
        String nombre = almacenar.nextLine();
 
-       System.out.println("El nuevo nombre es: " + nombre);
+       System.out.println("El nuevo nombre es: "+nombre);
 
     }
 }
