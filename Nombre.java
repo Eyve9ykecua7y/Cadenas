36966fa522751c7db07e94c1dd6261547c9800e3
Eyve9ykecua7y <20243rd049@utez.edu.mx>
2025-05-08 11:56:54 -0600

    corregi el nombre de clase

--- Nombre.java
+++ Nombre.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Nombre_Completo{
+public class Nombre{
     public static void main(String args[]){
         Scanner almacenar = new Scanner(System.in);
        System.out.println("Escribe tu nombre: ");
@@ -9,7 +9,7 @@ public class Nombre_Completo{
        System.out.println("Escribe tu apellido materno: ");
        String apellidoMaterno = almacenar.nextLine();
 
-       System.out.println("El nuevo nombre completo es : "+nombre + apellidoPaterno + apellidoMaterno);
+       System.out.println("El nuevo nombre completo es : "+nombre +" "+ apellidoPaterno +" "+ apellidoMaterno);
 
     }
 }
